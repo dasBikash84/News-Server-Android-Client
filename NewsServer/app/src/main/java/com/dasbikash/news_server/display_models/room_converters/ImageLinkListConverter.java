550c7cb3af295dce479a1ac8f20ce6d42b7e6ead
Bikash Das <bikashdaseee@gmail.com>
2019-03-27 21:14:34 +0600

    Model classes created for view controllers. This closes #1

---
+++ NewsServer/app/src/main/java/com/dasbikash/news_server/display_models/room_converters/ImageLinkListConverter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 www.dasbikash.org. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dasbikash.news_server.display_models.room_converters;
+
+import com.dasbikash.news_server.display_models.ImageLinkList;
+import com.dasbikash.news_server.display_models.NewsCategoryEntry;
+
+import androidx.room.TypeConverter;
+
+public class ImageLinkListConverter {
+
+    private static String DATA_BRIDGE = "!@#$";
+
+    @TypeConverter
+    public static String fromImageLinkList(ImageLinkList imageLinkList){
+
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = 0; i < imageLinkList.getImageLinks().size(); i++) {
+            stringBuilder.append(i);
+            if (i!= imageLinkList.getImageLinks().size()-1){
+                stringBuilder.append(DATA_BRIDGE);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @TypeConverter
+    public static ImageLinkList toImageLinkList(String imageLinkListString){
+
+        ImageLinkList imageLinkList = new ImageLinkList();
+
+        if (imageLinkListString!=null){
+            for (String entryStr : imageLinkListString.split(DATA_BRIDGE)){
+                imageLinkList.getImageLinks().add(entryStr);
+            }
+        }
+
+        return imageLinkList;
+    }
+}
---
+++ NewsServer/app/src/main/java/com/dasbikash/news_server/display_models/room_converters/NewsCategoryEntryConverter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 www.dasbikash.org. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dasbikash.news_server.display_models.room_converters;
+
+import com.dasbikash.news_server.display_models.NewsCategoryEntry;
+
+import java.util.ArrayList;
+
+import androidx.room.TypeConverter;
+
+public class NewsCategoryEntryConverter {
+
+    private static String DATA_BRIDGE = ",";
+
+    @TypeConverter
+    public static String fromNewsCategoryEntry(NewsCategoryEntry entry){
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (int i = 0; i < entry.getEntries().size(); i++) {
+            stringBuilder.append(i);
+            if (i!= entry.getEntries().size()-1){
+                stringBuilder.append(DATA_BRIDGE);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @TypeConverter
+    public static NewsCategoryEntry toNewsCategoryEntry(String entryListString){
+        NewsCategoryEntry entry = new NewsCategoryEntry();
+
+        if (entryListString!=null){
+            for (String entryStr : entryListString.split(DATA_BRIDGE)){
+                try {
+                    entry.getEntries().add(Integer.parseInt(entryStr));
+                }catch (NumberFormatException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return entry;
+    }
+}
