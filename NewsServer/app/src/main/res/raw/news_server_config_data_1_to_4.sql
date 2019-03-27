f4f4cab9cd5ddc5d4a1e81eb53de6c9f180cf1b4
Bikash Das <bikashdaseee@gmail.com>
2019-03-27 06:02:24 +0600

    Old project files untracked

--- NewsServer/app/src/main/res/raw/news_server_config_data_1_to_4.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('বণিক বার্তা',15,1);
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('ভোরের কাগজ',16,1);
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('New Age',17,1);
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('Daily Sun',18,1);
-UPDATE feature_group SET title = 'কালের কণ্ঠ' WHERE id = 28;
-INSERT INTO settings_value (id,description,value) VALUES (5,'Article text font size',16);
--- NewsServer/app/src/main/res/raw/news_server_config_data_2_to_4.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('New Age',17,1);
-INSERT INTO feature_group (title,category_identifier,is_active) VALUES ('Daily Sun',18,1);
-UPDATE feature_group SET title = 'কালের কণ্ঠ' WHERE id = 28;
-INSERT INTO settings_value (id,description,value) VALUES (5,'Article text font size',16);
